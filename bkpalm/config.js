c1ac0fbd996decb710931ab16db6e11ad53f2e39
RTF <rasheim.freeman@publicissapient.com>
2021-03-02 21:49:51 -0500

    BK palm scaffold(1)

---
+++ bkpalm/config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Readonly', // <title>
+  manifestName: 'Readonly',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-readonly/`, // This path is subpath of your hosting https://domain/portfolio
+  authorName: 'Davoria Cooper',
+  heading: "Future home of Brooklyn Palm",
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:davoria@gmail.com',
+    },
+  ],
+};
